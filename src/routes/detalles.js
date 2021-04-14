4742cd5b7691b8bcca6b5a6981bad5d679bd0ffe
shelkanife <81986306+shelkanife@users.noreply.github.com>
2021-04-14 17:16:22 -0500

    Delete src directory

--- src/routes/detalles.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const router = Router()
-const materiasController = require('../controllers/detalles.controller')
-
-router.get('/:id_proyecto',
-    materiasController.findDetalles);
-
-module.exports=router;
--- src/routes/proyectos.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const router = Router()
-const materiasController = require('../controllers/proyectos.controller')
-
-router.get('/:NRC',
-    materiasController.findProyectos);
-
-module.exports=router;

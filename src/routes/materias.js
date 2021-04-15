22c274ae95dbb43120371ff2ba1cbcc1030d69c3
shelkanife <81986306+shelkanife@users.noreply.github.com>
2021-04-14 21:44:10 -0500

    Delete src directory

--- src/routes/materias.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const router = Router()
-const materiasController = require('../controllers/materias.controller')
-
-router.get('/:id_usuario',
-    materiasController.findMaterias);
-
-module.exports=router;

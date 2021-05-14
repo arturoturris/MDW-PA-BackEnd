314b7093d2bd5549df64768b43b64a75ad940f56
shelkanife <81986306+shelkanife@users.noreply.github.com>
2021-05-14 18:41:32 -0500

    Delete src directory

--- src/routes/carreras.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const router = Router()
-const carrerasController = require('../controllers/carreras.controller')
-
-router.get('/',
-    carrerasController.getCarreras)
-
-module.exports = router
--- src/routes/equipo.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const router = Router({mergeParams: true})
-const equipoController = require('../controllers/equipo.controller')
-
-router.route('/')
-    .get(equipoController.getIntegrantes)
-    .post(equipoController.agregarIntegrante)
-    .delete(equipoController.eliminarIntegrante)
-module.exports = router

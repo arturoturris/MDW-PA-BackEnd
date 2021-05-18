361b197c2704af0a421f13f3efddaf4a64b3b203
AlanMarcial16 <48525205+AlanMarcial16@users.noreply.github.com>
2021-05-18 10:37:17 -0500

    Update tareas.controller.js

--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -1,90 +1,145 @@
-const {sequelize} = require('../config/sequelize.js')
-const {DataTypes} = require('sequelize')
+const {sequelize} = require('../config/sequelize')
+const {handleError} = require('./error.controller')
+const ModelValidator = require('../validator/ModelValidator')
 
-const Tarea = sequelize.define('Tarea',{
-    id_tarea: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    nombre_tarea: {
-        type: DataTypes.STRING(45),
-        allowNull: false,
-        validate: {
-            notNull: {
-                args: true,
-                msg: 'El nombre debe ser proporcionado.'
-            },
-            len:{
-                args: [5,45],
-                msg: 'Pocos o demasiados carácteres.'
-            }
-        }
-    },
-    fecha_inicio:{
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-        defaultValue:  DataTypes.DATE.NOW,
-        validate: {
-            notNull: {
-                args: true,
-                msg: 'La fecha de inicio debe ser proporcionada.'
-            },
-            isDate: {
-                args: true,
-                msg: 'La fecha de inicio no contiene un formato válido.'
-            }
-        }
-    },
-    fecha_limite:{
-        type: DataTypes.DATEONLY,
-        allowNull: true,
-        validate: {
-            isDate: {
-                args: true,
-                msg: 'La fecha límite no contiene un formato válido.'
-            }
-        }
-    },
-    fecha_fin:{
-        type: DataTypes.DATEONLY,
-        allowNull: true,
-        validate: {
-            isDate: {
-                args: true,
-                msg: 'La fecha fin no contiene un formato válido.'
-            }
-        }
-    },
-    descripcion:{
-        type: DataTypes.STRING(200),
-        allowNull: true,
-        validate: {
-            len: {
-                args: [0,200],
-                msg: 'Pocos o demasiados caracteres.'
-            }
-        }
-    },
+function existsTarea(req,res,next){
+    sequelize.models.Tarea.findOne({where: {id_tarea: req.params.id_tarea}})
+        .then(tarea => {
+            if(tarea)
+                next()
+            else
+                res.sendStatus(404)
+        })
+        .catch(err =>
+            handleError(req,res,err))   
+}
 
-},{
-    tableName: 'tarea',
-    timestamps: false,
-    hooks: {
-        beforeSave:(tarea,options) => {
-            tarea.set('nombre_tarea',tarea.get('nombre_tarea').toUpperCase())
-            tarea.set('descripcion',tarea.get('descripcion').toUpperCase())
-        }
-    }
-})
-
-Tarea.associate = function(models){
-    
-    models.Tarea.belongsToMany(models.Alumno,{
-        through: models.Equipo,
-        foreignKey: 'id_tarea'
+function buildTarea(body){
+    return sequelize.models.Tarea.build({
+        nombre_tarea: body.nombre_tarea,
+        fecha_inicio: body.fecha_inicio,
+        fecha_limite: body.fecha_limite || null,
+        fecha_fin: body.fecha_fin || null,
+        descripcion: body.descripcion
     })
 }
 
-module.exports = Tarea
+async function createTarea(req,res){
+    let tarea = buildTarea(req.body)
+    console.log('****TAREA***')
+    console.log(req.body)
+
+    try{    
+        await sequelize.transaction(async t => {
+            tarea = await tarea.save({transaction: t,validate: false})
+        })
+
+        return res.sendStatus(201)
+    }
+    catch(err){
+        handleError(req,res,err)
+    }
+}
+
+function validateTarea(requestType){
+    return async (req,res,next) => {
+        let tarea = buildTarea(req.body)
+
+        let validator = new ModelValidator()
+
+        try{
+            let skip = ['id_tarea']
+
+            if(requestType === 'put')
+                skip = ['id_tarea']
+
+            await validator.validate(tarea,{skip})
+        }
+        catch(err){
+            return handleError(req,res,err)
+        }
+        
+        let validationErrors = validator.getErrors()
+
+        if(validationErrors)
+            return res.status(422).json({errors: validationErrors})
+
+        next()
+    }
+}
+
+
+function findDetalles(req,res){
+    const {id_tarea} = req.params;
+    
+    sequelize.models.Tarea.findOne({
+        include: {
+            attributes: [],
+                include: {
+                    model: sequelize.models.Tarea
+                }
+        },
+        attributes: [
+            'id_tarea',
+            'nombre_tarea',
+            'fecha_inicio',
+            'fecha_limite',
+            'fecha_fin',
+            'descripcion'
+        ],
+        where: {id_tarea}
+    })
+
+    .catch(err => {
+        handleError(req,res,err)
+    })
+}
+
+async function updateTarea(req,res){
+    try{
+        let tarea = await sequelize.models.Tarea.findOne({where: {id_tarea: req.params.id_tarea}})
+
+        await sequelize.transaction(async t => {
+            
+            await tarea.update({
+                nombre_tarea: req.body.nombre_tarea,
+                fecha_inicio: req.body.fecha_inicio,
+                fecha_limite: (req.body.fecha_limite || null),
+                fecha_fin: (req.body.fecha_fin || null),
+                descripcion: req.body.descripcion
+            },{
+                transaction: t,
+                validate: false
+            })
+        })
+
+        res.sendStatus(200)
+    }
+    catch(err){
+        handleError(req,res,err)
+    }
+}
+
+async function deleteTarea(req,res){
+    try{
+        const tarea = await sequelize.models.Tarea.findOne({where: {id_tarea: req.params.id_tarea}})
+
+        await sequelize.transaction(async t => {
+            await tarea.destroy({transaction: t})
+        })
+
+        res.sendStatus(200)
+    }
+    catch(err){
+        handleError(req,res,err)
+    }
+}
+
+module.exports = {
+    validateTarea,
+    createTarea,
+    findDetalles,
+    updateTarea,
+    existsTarea,
+    deleteTarea
+}
